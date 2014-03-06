da210b4cf8f537960c8840faa5af87e536491cef
DanSnow <dododavid006@gmail.com>
2014-03-06 14:13:44 +0800

    Add notification;Use config interface

--- lib/dormflows.py
+++ lib/dormflows.py
@@ -4,18 +4,43 @@
 import sys
 import os
 import subprocess
-
-import netifaces
-import notify2
+from datetime import datetime
 
 from PyQt4.QtGui import *
 from PyQt4.QtCore import *
 
+import util
 from dormflows_ui import Ui_DormFlows
 from FlowsGetter import FlowsGetter
 from SysTray import SystemTrayIcon
 from config import Config
+from notifier import Notifier
 
+
+class NotifyFlow:
+  NotifyFlows = [7000, 7500, 8000, 8300]
+  def __init__(self):
+    self.flowIter = iter(self.NotifyFlows)
+    self.notifyFlows = next(self.flowIter)
+    self.notifier = Notifier()
+    self.needUpdate = True
+
+  def checkAndNotify(self, flow):
+    if datetime.now().hour == 0 and self.needUpdate:
+      self.flowIter = iter(self.NotifyFlows)
+      self.needUpdate = False
+      self._nextFlow()
+    else:
+      self.needUpdate = True
+    if flow > self.notifyFlows:
+      self.notifier.notify("Notice: Now flow is {0}".format(flow))
+      self._nextFlow()
+
+  def _nextFlow(self):
+    try:
+      self.notifyFlows = next(self.flowIter)
+    except StopIteration:
+      pass
 
 class DromFlows(QDialog):
   def __init__(self, parent = None):
@@ -38,9 +63,12 @@ class DromFlows(QDialog):
     self.trayIcon.exitAction.triggered.connect(self._appExit)
     self.trayIcon.show()
 
+    #init notifier
+    self.notify = NotifyFlow()
+
     # init ui
     self.ui.imgL.setPixmap(self.okPic)
-    self.ui.disL.setText("M時停用\"{0}\"".format(self.interface))
+    self.ui.disL.setText("M時停用\"{0}\"".format(self._config["interface"]))
     self.ui.limitLE.setValidator(QIntValidator(0, 9999, self))
 
     # connect signal
@@ -68,7 +96,6 @@ class DromFlows(QDialog):
 
   def initInterfaceInfo(self):
     self.interface = "eth0"
-    self.interfacesList = netifaces.interfaces()
 
   def loadThemeIcon(self):
     self.okPic = QIcon.fromTheme("emblem-default").pixmap(150)
@@ -78,35 +105,39 @@ class DromFlows(QDialog):
   def showFlows(self):
     flowsGetter = FlowsGetter(self.ip)
     self.flows = flowsGetter.flows
-    if self.flows:
+    if self.flows is not None:
       self.ui.flowsL.setText("目前流量： {0} MB".format(self.flows))
       self.ui.flowsProg.setValue(self.flows)
+      self.notify.checkAndNotify()
       if self.flows > 7000:
         self.ui.imgL.setPixmap(self.urgentPic)
       else:
         self.ui.imgL.setPixmap(self.okPic)
       self._disableInterface()
     else:
-      self.ui.flowsL.setText("不明原因錯誤(有可能是IP位置錯了)")
+      if util.checkConnect(self._config["interface"]):
+        self.ui.flowsL.setText("\"{0}\" isn't connect".format(self._config["interface"]))
+      else:
+        self.ui.flowsL.setText("不明原因錯誤(有可能是IP位置錯了)")
       self.ui.imgL.setPixmap(self.deadPic)
 
   def showIP(self):
-    try:
-      self.ip = netifaces.ifaddresses(self.interface)[netifaces.AF_INET][0]['addr']
+    if util.checkConnect(self._config["interface"]):
+      self.ip = util.getIP(self._config["interface"])
       self.ui.ipLE.setText(self.ip)
       self.showFlows()
       self.timer = QTimer()
       self.timer.timeout.connect(self.showFlows)
       self.timer.start(self._config["checkDelay"])
-    except KeyError:
+    else:
       self.ip = None
       self.ui.imgL.setPixmap(self.deadPic)
-      self.ui.flowsL.setText("\"{0}\" isn't connect".format(self.interface))
+      self.ui.flowsL.setText("\"{0}\" isn't connect".format(self._config["interface"]))
 
   def _disableInterface(self):
     if self.limit and self.chkStat:
       if self.flows > self.limit:
-        subprocess.call(["nmcli", "d", "disconnect", "iface", self.interface, "--nowait"], shell = False)
+        subprocess.call(["nmcli", "d", "disconnect", "iface", self._config["interface"], "--nowait"], shell = False)
 
   def _appExit(self):
     """call when application exit
