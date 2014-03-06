a52103a4fdb89f7d3ba8ad79944c73a1bc28e3b2
DanSnow <dododavid006@gmail.com>
2014-03-06 14:14:10 +0800

    Add timeout porpery

--- lib/notifier.py
+++ lib/notifier.py
@@ -7,11 +7,19 @@ from datetime import datetime
 import notify2
 
 class Notifier:
-  def __init__(self, timeout):
+  def __init__(self, timeout = 5000):
     self.flowPicUrl = "file://" + os.path.join(os.path.dirname(os.path.realpath(__file__)), "flow.png")
     notify2.init("DormFlows")
     self._notify = notify2.Notification("DormFlows", "", self.flowPicUrl)
     self._notify.timeout = timeout
+
+  def timeout():
+    def fget(self):
+      return self._notify.timeout
+    def fset(self, value):
+      self._notify.timeout = value
+    return locals()
+  timeout = property(**timeout())
 
   def notify(self, message):
     self._notify.update("DormFlows", message, self.flowPicUrl)
@@ -19,4 +27,6 @@ class Notifier:
 
 if __name__ == '__main__':
   n = Notifier(5000)
+  n.timeout = 1000
+  print(n.timeout)
   n.notify("Hello")
